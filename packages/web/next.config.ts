4bc5388955e9532a9b31a6f9f679677f7d770d50
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-07-20 11:05:22 +0700

    update next.config.ts

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -1,14 +1,22 @@
 import type { NextConfig } from 'next';
 
 const NODE_ENV = process.env.NODE_ENV ?? 'development';
+const VERCEL_ENV = process.env.VERCEL_ENV ?? 'development';
+
+const partialNextConfig: Partial<NextConfig> =
+  VERCEL_ENV === 'development'
+    ? {
+        basePath: NODE_ENV === 'development' ? '' : '/editor',
+        output: NODE_ENV === 'development' ? 'standalone' : 'export',
+        distDir: NODE_ENV === 'development' ? '.next' : '../../docs',
+      }
+    : {};
 
 const nextConfig: NextConfig = {
   /* config options here */
+  ...partialNextConfig,
   trailingSlash: true,
   reactStrictMode: true,
-  basePath: NODE_ENV === 'development' ? '' : '/editor',
-  output: NODE_ENV === 'development' ? 'standalone' : 'export',
-  distDir: NODE_ENV === 'development' ? '.next' : '../../docs',
 };
 
 export default nextConfig;
